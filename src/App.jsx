a27a50ccfac8e744f8e0ea915f6e6fed3400e083
yousof-ali <yousofali293@gmail.com>
2024-08-24 18:55:21 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>simple CRUD</h1>  
+
+
+      <form>
+        <input type="text" name="name" placeholder='Name' />
+        <input type="email" name="email" placeholder='Email'  />
+        <input type="submit" value={"submit"}  />
+      </form>
+    </>
+  )
+}
+
+export default App
